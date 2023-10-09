728e0329233d6d96409b3e9f8a74c240ab50c271
EDWAR GONZALEZ <yercken30@gmail.com>
2023-10-09 16:42:53 -0500

    + QUERY DE TABLA BASE

---
+++ QUERYS.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Parking (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    park1 char(1),
+    park2 char(1),
+    park3 char(1),
+    park4 char(1),
+    park5 char(1),
+    park6 char(1),
+    park7 char(1),
+    park8 char(1),
+    park9 char(1),
+    park10 char(1),
+    park11 char(1)
+);
