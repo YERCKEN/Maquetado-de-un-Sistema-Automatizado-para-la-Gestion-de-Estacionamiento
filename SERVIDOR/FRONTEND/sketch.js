924e072b1058b3d9834a186948805d19a6347664
Jeisson Paredes <jeisonparedes63@gmail.com>
2023-10-22 18:01:47 -0500

    Dibujado de estacionamiento cada segundo, con los valores de la base de datos

--- SERVIDOR/FRONTEND/sketch.js
+++ SERVIDOR/FRONTEND/sketch.js
@@ -20,6 +20,8 @@ let canvasY = 0; // Posición Y actual del canvas
 // % DE ALTURA DEL CANVAS
 let porcentajeDeAltura = 0.80;
 
+//DATOS DE ESTACIONAMIENTO
+let parkingData;
 
 //PRELOAD __________________________________________________________________________-
 
@@ -33,8 +35,8 @@ function setup() {
 
   // Cambiar el alto del canvas según el porcentaje
   let canvas = createCanvas(windowWidth, windowHeight * porcentajeDeAltura);
-
-
+  fetchParkingData();
+  setInterval(fetchParkingData, 1000);  // Llama a fetchParkingData cada 1000ms (es decir, cada segundo)
   //canvas.parent('canvas-container'); // Agrega el canvas a un contenedor HTML
 }
 
@@ -51,14 +53,38 @@ function draw() {
 
   // Imagen
   image(imgFondo, 0, 0);
-
+  if (parkingData) {
+    dibujarEstacionamientoC();
+  }
+/*
   // Dibujar rectángulos ===============================================
+  //Estacionamiento C
   dibujarRectangulo(683, 222, 44, 91);
   dibujarRectangulo(728, 222, 44, 91);
-
+  dibujarRectangulo(774, 222, 44, 91);
+  dibujarRectangulo(819, 222, 44, 91);
+  dibujarRectangulo(864, 222, 43, 91);
+  dibujarRectangulo(909, 222, 43, 91);
+  dibujarRectangulo(683, 320, 43, 91);
+  dibujarRectangulo(728, 320, 43, 91);
+  dibujarRectangulo(774, 320, 43, 91);
+  dibujarRectangulo(819, 320, 43, 91);
+  dibujarRectangulo(864, 320, 43, 91);
+  dibujarRectangulo(909, 320, 43, 91);
+  //Estacionamiento A
+  dibujarRectangulo(77, 1135, 86, 51);
+  dibujarRectangulo(77, 1190, 86, 51);
+  dibujarRectangulo(77, 1245, 86, 51);
+  dibujarRectangulo(77, 1298, 86, 51);
+  dibujarRectangulo(77, 1351, 86, 51);
+  dibujarRectangulo(77, 1405, 86, 53);
+  //Estacionamiento B
+  dibujarRectangulo(601, 931, 42, 87);
+  dibujarRectangulo(647, 931, 43, 87);
+  dibujarRectangulo(693, 931, 42, 87);
+  dibujarRectangulo(739, 931, 42, 87);*/
   // FIN DE TRANSFORMACIONES ===============================================================
   pop();
-  
 
   // Información de posición del mouse
   textSize(24);
@@ -70,16 +96,39 @@ function draw() {
 
   agregarMargenes();
 }
+function fetchParkingData() {
+  fetch('https://phppasarvariable.000webhostapp.com/database_access.php')
+    .then(response => response.json())
+    .then(data => {
+        parkingData = data;
+        //dibujarEstacionamientoC();
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+function dibujarEstacionamientoC() {
+  let positions = [
+    {x: 683, y: 222}, {x: 728, y: 222}, {x: 774, y: 222}, {x: 819, y: 222}, {x: 864, y: 222}, {x: 909, y: 222},
+    {x: 683, y: 320}, {x: 728, y: 320}, {x: 774, y: 320}, {x: 819, y: 320}, {x: 864, y: 320}, {x: 909, y: 320}
+  ];
+  
+  for (let i = 0; i < positions.length; i++) {
+    let parkKey = 'park' + (i + 1);
+    let parkValue = parkingData[parkKey];
+    console.log(`Valor para ${parkKey}: ${parkValue}`); // Imprime el valor
+    dibujarRectangulo(positions[i].x, positions[i].y, 43, 91, parkValue);
 
+  }
+}
 // FUCNIÓN PAR ADIBUJAR RECTÁNGULOO
-function dibujarRectangulo(x, y, ancho, alto) {
-  //push(); // Guardamos la configuración actual de transformación
-  
-  // Dibujar un rectángulo azul en la posición (x, y) con el ancho y alto especificados
-  fill("#C4FFEB");
+function dibujarRectangulo(x, y, ancho, alto, value) {
+  if (value === '0') {
+    fill("#FF0000");
+  } else if (value === '1') {
+    fill("#C4FFEB");
+  }
   rect(x, y, ancho, alto);
-  
-  //pop(); // Restauramos la configuración de transformación anterior
 }
 
 
